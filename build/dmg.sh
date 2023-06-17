2c7f9fab319f8f7c201b6212575a600fb6e0ef82
duolabmeng6 <1715109585@qq.com>
2023-06-17 11:14:34 +0800

    Update dmg.sh
    mac安装包

--- build/dmg.sh
+++ build/dmg.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-# 定义应用程序名称和版本号
 APP_NAME="qoq"
-# 环境变量 version
 APP_VERSION="$version"
-
-# 创建临时目录和镜像文件名
-TEMP_DIR="./"
 DMG_NAME="${APP_NAME}-${APP_VERSION}.dmg"
+TEMP_DIR="./install"
+
+mkdir -p "${TEMP_DIR}"
+
+cp -r "./${APP_NAME}.app" "${TEMP_DIR}"
 
 # 创建 Applications 文件夹的快捷方式
 ln -s "/Applications" "${TEMP_DIR}/Applications"
 
 # 创建空的镜像文件
 hdiutil create -srcfolder "${TEMP_DIR}" -volname "${APP_NAME}" -format UDZO "${DMG_NAME}"
-
-# 删除临时目录
-rm -rf "${TEMP_DIR}"
 
 echo "${DMG_NAME} 已经创建完成！"
