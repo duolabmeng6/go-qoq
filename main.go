23c65a0c1cf209ab3f7408b35ca283f15558505f
duolabmeng6 <1715109585@qq.com>
2023-06-21 13:33:19 +0800

    命名优化

--- main.go
+++ main.go
@@ -506,7 +506,7 @@ func 绑定热键(app *application.App, window *application.WebviewWindow) {
 		robotgo.EventHook(hook.KeyUp, []string{}, func(e hook.Event) {
 			//ecore.E调试输出("G按下的热键列表", G按下的热键列表)
 			// Esc 关闭窗口
-			if e.Keychar == rune(65535) {
+			if mymodel.ContainsAll(G按下的热键列表, "escape") {
 				window.Hide()
 			}
 			if mymodel.ContainsAll(G按下的热键列表, AppData.划词翻译快捷键...) {
